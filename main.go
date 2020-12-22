338fb42a1c38d471cfe039afd4ff26e860b9fafb
wjl120 <wjl120wjl@163.com>
2020-12-22 21:39:39 +0800

    main.go

--- main.go
+++ main.go
@@ -57,6 +57,7 @@ func main() {
 	}
 
 }
+// 
 func init() {
 	flag.StringVar(&kubeconfig, "kubeconfig", "", "Path to a kubeconfig. Only required if out-of-cluster.")
 	flag.StringVar(&masterURL, "master", "", "The address of the Kubernetes API server. Overrides any value in kubeconfig. Only required if out-of-cluster.")
